65b4bd3ddc29c208fa12fa48227a0292338dca65
Daniel Gutierrez <cdanielgl@gmail.com>
2023-07-12 13:55:38 -0600

    code added

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,65 @@
-const analyzer = {  
+const regex = /[\W_]/g;
+
+function getNums(string) {
+  if (!string.trim().length) {
+    return [];
+  }
+  const nums = string
+    .trim()
+    .split(" ")
+    .map((x) => x.replace(" ", ""))
+    .map(Number)
+    .filter((x) => !isNaN(x));
+  return nums;
+}
+
+function getWords(text) {
+  const aux = text.replace(regex, "");
+  if (!aux.length) {
+    return [];
+  }
+  const words = text.split(" ").filter(function (word) {
+    return word.length > 0;
+  });
+  return words;
+}
+
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return getWords(text).length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text.replace(regex, "").length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = getWords(text);
+
+    if (!words.length) {
+      return 0;
+    }
+
+    return parseFloat(
+      (
+        words.reduce((acc, current) => acc + current.length, 0) / words.length
+      ).toFixed(2)
+    );
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    return getNums(text).length;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    return getNums(text).reduce(function (acc, current) {
+      return acc + current;
+    }, 0);
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const resetButton = document.getElementById("reset-button");
+const textArea = document.getElementsByName("user-input")[0];
+const statsLi = document.getElementsByTagName("li");
+
+function displayStat(index, title, value) {
+  const element = statsLi[index];
+  element.textContent = `${title}: ${value}`;
+}
+
+function displayStats(text) {
+  displayStat(0, "Caracteres", analyzer.getCharacterCount(text));
+  displayStat(
+    1,
+    "Caracteres sin espacios",
+    analyzer.getCharacterCountExcludingSpaces(text)
+  );
+  displayStat(2, "Palabras", analyzer.getWordCount(text));
+  displayStat(3, "Numeros", analyzer.getNumberCount(text));
+  displayStat(4, "Suma numeros", analyzer.getNumberSum(text));
+  displayStat(5, "Promedio longitd", analyzer.getAverageWordLength(text));
+}
+
+textArea.addEventListener("keyup", function () {
+  const text = textArea.value;
+  displayStats(text);
+});
+
+function resetStats() {
+  displayStats("");
+}
+
+resetButton.addEventListener("click", function () {
+  textArea.value = "";
+  resetStats();
+});
